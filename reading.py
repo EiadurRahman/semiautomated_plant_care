70eddb652fc42d681ed601dadbb40053eea0d24b
EiadurRahman <77844245+EiadurRahman@users.noreply.github.com>
2024-02-09 17:16:15 +0600

    added humd_temp func
    added ability to measure temperature and humidity with dht11 sensor

--- reading.py
+++ reading.py
@@ -1,11 +1,18 @@
-from machine import ADC
+from machine import ADC,Pin
 from time import sleep
+import dht
 
+hts = dht.DHT11(Pin(4))
 # moisture sensor reading
 def moist():
     adc_value = ADC(0).read()
     return str(adc_value)
-    
-# humidity sensor reading
 
-# temperature sensor reading
+def humd_temp(keyword):
+    hts.measure()
+    sleep(1)
+    if keyword == 'temp':
+        return_value = str(hts.temperature())
+    elif keyword == 'humd':
+        return_value = str(hts.humidity())
+    return return_value
